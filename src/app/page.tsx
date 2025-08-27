bd4b4cf888e22b2087a50583a3e38ae4bb1611f8
Joodi <winbeta.com@gmail.com>
2025-08-27 22:46:02 +0330

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
 
 console.log("another test"); // یه خط جدید اضافه کن
-git commit -m "test husky"
-  
+
   return (
     <div>
       test
